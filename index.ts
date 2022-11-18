aa704f0b5aefc62c20e0bbeb36060a8585db2da3
CarlonBaird <carlyestrella@live.com>
2022-11-18 10:37:01 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,28 @@
 import { Observable } from 'rxjs';
 
-const observable$ = new Observable((subscriber) => {});
+const observable$ = new Observable<string>((subscriber) => {
+  console.log('Observable executed!');
+  subscriber.next('Alice');
+  subscriber.next('Tim');
+  setTimeout(() => {
+    subscriber.next('Chris');
+    //emitting the complete notification after the third value has been emitted.
+    subscriber.complete();
+  }, 2000);
 
-observable$.subscribe();
+  return () => {
+    console.log('Teardown'); //teardown phase of the subscription
+  };
+});
+
+console.log('Before subscribe');
+// observable$.subscribe((value) => {
+//   console.log(value);
+// });
+
+//we have to use this format to deal with complete
+observable$.subscribe({
+  next: (value) => console.log(value),
+  complete: () => console.log('Completed'),
+});
+console.log('After subscribe');
