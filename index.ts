20357b5e922b19a1b5c4504c7267c3a2e33ab31c
CarlonBaird <carlyestrella@live.com>
2022-11-18 11:42:14 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -4,11 +4,11 @@ const observable$ = new Observable<string>((subscriber) => {
   console.log('Observable executed!');
   subscriber.next('Alice');
   subscriber.next('Tim');
-  setTimeout(() => {
-    subscriber.next('Chris');
-    //emitting the complete notification after the third value has been emitted.
-    subscriber.complete();
-  }, 2000);
+  // setTimeout(() => {
+  //   subscriber.next('Chris');
+  //   //emitting the complete notification after the third value has been emitted.
+  //   subscriber.complete();
+  // }, 2000);
 
   //emit error 4 seconds after subscribing
   setTimeout(() => subscriber.error(new Error('Failure')), 4000);
